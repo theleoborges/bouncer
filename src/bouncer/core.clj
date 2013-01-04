137acfbdbe62cdedcca83f7a56edd45487ef8f13
Leonardo Borges <leonardoborges.rj@gmail.com>
2013-01-04 16:13:55 +1100

    New macro defvalidator that simplifies the creation of new validating functions. Refactored built-in validators to be implemented using defvalidator

--- src/bouncer/core.clj
+++ src/bouncer/core.clj
@@ -33,43 +33,107 @@ A validator can also be marked optional, in which case the validation will only 
    (vector? k) (name (peek k))
    (keyword? k) (name k)))
 
+
+(defmacro defvalidator
+  "Defines a new validating functions using args & body semantics as provided by \"defn\".
+  docstring and opts-map are optional
+
+  opts-map is a map of key-value pairs and may be one of:
+    :default-message-format used when the client of this validator doesn't provide a message
+    :optional whether the validation should be run only if the given key has a non-nil value in the map. Defaults to false.
+"
+  {:arglists '([name docstring? opts-map? [args*] & body])}
+  [name & options]
+  (let [docstring (if (string? (first options))
+                    (first options)
+                    nil)
+        options (if (string? (first options))
+                  (next options)
+                  options)
+        {:keys [default-message-format optional]} (if (map? (first options))
+                                                    (first options)
+                                                    {})
+        options (if (map? (first options))
+                  (next options)
+                  options)
+        [args & body] options]
+    `(defn ~(with-meta name {:doc docstring})
+       {:arglists '([~(symbol 'k) ~(symbol 'opts?)])}
+       ([k#]
+          (~name k# :message (format (if ~default-message-format
+                                       ~default-message-format
+                                       "Custom validation failed for %s") (key->name k#))))
+       ([k# & {message# :message}]
+          (mk-validator (fn ~args
+                          ~@body) k# message# :optional ~optional)))))
+
 ;; Default validators
-(defn required
-  "Returns a validation function that checks k has a value the map being validated.
-If the value is a string, it makes sure it's not empty, otherwise it checks for nils.
+(defvalidator required
+  "Validating function that checks if its only argument has a value.
+
+  If the value is a string, it makes sure it's not empty, otherwise it checks for nils.
+
+  The resulting function takes a key k.
+
+  If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
+
+  opts is an optional sequence of named arguments and may be one of:
+    - :message the message to be added to the :errors map should this validation fail
+  
+  If no message is given, a default message will be used
+"
+  {:default-message-format "%s must be present"}
+  [value]
+  (if (string? value)
+    (not (empty? value)) 
+    (not (nil? value))))
+
+(defvalidator number
+  " Validating function that checks its only argument is a number.
+
+  The resulting function takes a key k.
+
+  If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
+
+  opts is an optional sequence of named arguments and may be one of:
+    - :message the message to be added to the :errors map should this validation fail
+  
+  If no message is given, a default message will be used
+"
+  {:default-message-format "%s must be a number" :optional true}
+  [maybe-a-number]
+  (number? maybe-a-number))
+
+
+(defvalidator positive
+  " Validating function that checks its only argument is greater than.
+
+  The resulting function takes a key k.
+
+  If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
+
+  opts is an optional sequence of named arguments and may be one of:
+    - :message the message to be added to the :errors map should this validation fail
+  
+  If no message is given, a default message will be used
+"
+  {:default-message-format "%s must be a positive number" :optional true}
+  [number]
+  (and (number? number)
+       (> number 0)))
+
+(defn custom
+  "Returns a validation function that checks pred for every item in the collection at key k.
+
+pred can be any function that yields a boolean. It will be invoked for every item in the collection.
 
 If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
 
 If no message is given, a default message will be used"
-  ([k]
-     (required k :message (format "%s must be present" (key->name k))))
-  ([k & {message :message}]
-     (mk-validator #(if (string? %)
-                      (not (empty? %)) 
-                      (not (nil? %))) k message)))
-
-(defn number
-  "Returns a validation function that checks the value for k is a number.
-
-If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
-
-If no message is given, a default message will be used"
-  ([k]
-     (number k :message (format "%s must be a number" (key->name k))))
-  ([k & {message :message}]
-     (mk-validator number? k message :optional true)))
-
-(defn positive
-  "Returns a validation function that checks the value for k is greater than zero.
-
-If k is a vector, it is assumed to be the path in an nested associative structure. In this case, the :errors entry will mirror this path
-
-If no message is given, a default message will be used"
-  ([k]
-     (positive k :message (format "%s must be a positive number" (key->name k))))
-  ([k & {message :message}]
-     (mk-validator #(and (number? %)
-                         (> % 0)) k message :optional true)))
+  ([k pred]
+     (custom k pred :message (format "Custom validation failed for %s" (key->name k))))
+  ([k pred & {message :message}]
+     (mk-validator pred k message)))
 
 (defn every
   "Returns a validation function that checks pred for every item in the collection at key k.
@@ -99,63 +163,6 @@ Returns a vector where the first element is the map of validation errors if any 
   "Takes a map and one or more validation functions. Returns true if the map passes all validations. False otherwise"
   `(empty? (first (validate ~@args))))
 
-
-
-;; (core/validate invalid-map
-;;                (core/required :name)
-;;                (core/required :age)
-;;                (core/number :age)
-;;                (core/positive [:passport :number])
-;;                (core/every [:address :past]
-;;                            #(not (nil? (:country %)))))
-
-
-;; (core/validate invalid-map
-;;                :name core/required
-;;                :age  [core/required :message "custom"
-;;                       core/number   :message "custom"]
-;;                [:passport :number] core/positive
-;;                [:address :past]    [core/every #(not (nil? (:country %)))]
-;;                )
-
-;; (def my-rest [:name 'required
-;;               :age  ['required :message "custom"
-;;                      'number   :message "custom"]
-;;               [:passport :number] 'positive])
-
-
-
-;; (let [[key fn-or-vec & rest] my-rest]
-;;   (prn "key: " key " - fn-or-vec: " fn-or-vec))
-
-;; (for [rule (partition 2 my-rest)
-;;       :let [[key-or-vec fn-or-vec] rule]]
-;;   (cond
-;;    (vector? fn-or-vec) (prn `(something))
-;;    :else (prn `(~fn-or-vec ~key-or-vec))))
-
-
-;; (defn extract-options [v]
-;;   (apply hash-map (apply concat
-;;                    (take-while #(keyword? (first %))
-;;                                (partition 2 v)))))
-
-;; (defn but-options [v]
-;;   (flatten
-;;    (drop-while #(keyword? (first %))
-;;                (partition-all 2 v))))
-
-;; (extract-options [:pred #{} :message "alh" 'required 'number])
-;; (extract-options [:pred '#{} :message "alh" 'required :ka])
-;; (extract-options [:pred '#(not (nil? (:country %))) :message "alh" 'required 'number])
-;; (extract-options [:message "alh" 'required])
-;; (extract-options ['required])
-
-;; (but-options [:pred #{} :message "alh" 'required 'number])
-;; (but-options [:pred #{} :message "alh" 'required :ka])
-;; (but-options [:message "alh" 'required])
-;; (but-options ['required])
-
 (defn build-multi-step
   ([key-or-vec fn-vec] (build-multi-step key-or-vec fn-vec []))
   ([key-or-vec [f-or-list & rest] acc]
@@ -171,8 +178,6 @@ Returns a vector where the first element is the map of validation errors if any 
                      rest
                      (conj acc `(~(resolve f-or-list) ~key-or-vec)))))))
 
-;;(build-multi-step :name ['required :message "custom" 'number])
-
 (defn build-steps [forms]
   (reduce (fn [acc [key-or-vec fn-coll :as rule]]
             (cond
@@ -182,62 +187,27 @@ Returns a vector where the first element is the map of validation errors if any 
           []
           (partition 2 forms)))
 
-;;(build-steps my-rest)
-
-
-
-
 (defmacro validate [m & forms]
+  "Validates the map m using the validations specified by forms.
+
+  forms is a sequence of key/value pairs where:
+
+  key   ==> :keyword or [:a :path]
+
+  value ==> validation-function or
+           (validation-function args+opts) or
+           [validation-function another-validation-function] or
+           [validation-function (another-validation-function args+opts)]
+
+  e.g.:
+
+  (core/validate a-map
+               :name core/required
+               :age [core/required
+                     (core/number :message \"age must be a number\")]
+               [:passport :number] core/positive)
+
+  Returns a vector where the first element is the map of validation errors if any and the second is the original map (possibly)augmented with the errors map.
+"
   `(validate* ~m
              ~@(build-steps forms)))
-
-
-;; (macroexpand-1 '(validate {}
-;;                           :pets [(every #(not (nil? (:name %))))]))
-
-;; (macroexpand-1 '(validate {}
-;;                           :pets (every #(not (nil? (:name %))))))
-
-;; (validate {:pets [{:name nil}]}
-;;                           :pets (every #(not (nil? (:name %)))))
-
-;; (validate {:pets [{:name nil}]}
-;;                        :pets [(every #(not (nil? (:name %))))])
-
-;; (macroexpand-1
-;;  '(validate {} :name required
-;;                 :age  [(required :message "custom")
-;;                        number]
-;;                 [:passport :number] (positive :message "positive you idiot!")
-;;                 [:address :past] [(every #(not (nil? (:country %)))
-;;                                          :message "all past addresses must have a country")]))
-
-;; (validate {} :name required
-;;                 :age  [(required :message "custom")
-;;                        number]
-;;                 [:another :one] required
-;;                 [:passport :number] (positive :message "positive you idiot!")
-;;                 [:address :past] [(every #(not (nil? (:country %)))
-;;                                          :message "all past addresses must have a country")])
-
-;; (valid? {} :name required
-;;                 :age  [(required :message "custom")
-;;                        number]
-;;                 [:another :one] required
-;;                 [:passport :number] (positive :message "positive you idiot!")
-;;                 [:address :past] [(every #(not (nil? (:country %)))
-;;                                          :message "all past addresses must have a country")])
-
-;; (def person {:address {:past [{:country nil} {:country "Brazil"}]}})
-;; (new-validate person
-;;               :name required
-;;               :age  [required :message "custom"
-;;                      number]
-;;               [:passport :number] positive
-;;               [:address :past]    [every #(not (nil? (:country %)))
-;;                                          :message "all past addresses must have a country"])
-
-
-
-;; (def person {:age "leo"})
-
--- test/bouncer/core_test.clj
+++ test/bouncer/core_test.clj
@@ -1,7 +1,7 @@
 (ns bouncer.core-test
   (:use clojure.test)
   (:require [bouncer.core :as core]))
-(require '[bouncer.core :as core] :reload)
+
 (deftest validations
   (testing "Required validations"
     (is (not (core/valid? {}
@@ -25,7 +25,19 @@
     (is (core/valid? {:age nil}
                      :age core/number))
     (is (core/valid? {:age 10}
-                     :age core/number))))
+                     :age core/number)))
+
+  (testing "Custom validations"
+    (is (not (core/valid? {}
+                          :name (core/custom #((complement nil?) %)))))
+    (is (core/valid? {:name "Leo"}
+                     :name (core/custom #((complement nil?) %))))
+
+    (letfn [(not-nil [v] ((complement nil?) v))]
+      (is (not (core/valid? {}
+                            :name (core/custom not-nil))))
+      (is (core/valid? {:name "Leo"}
+                       :name (core/custom not-nil))))))
 
 (def map-no-street {:address {:street nil :country "Brazil"}})
 (def map-with-street (assoc-in map-no-street [:address :street]
@@ -67,44 +79,32 @@
 
 (deftest validation-messages
   (testing "default messages"
-    (is (= "name must be present"
-           (first-error-for :name
-                            (core/validate {}
-                                           :name core/required))))
-
-    (is (= "age must be present"
-           (first-error-for :age
-                            (core/validate {}
-                                           :age core/required))))
-    (is (= "age must be a number"
-           (first-error-for :age
-                            (core/validate {:age ""}
-                                           :age core/number))))
-
-    (is (= "age must be a positive number"
-           (first-error-for :age
-                            (core/validate {:age -7}
-                                           :age core/positive))))
-
     (is (= {
-            :age '("age must be a number" "age must be present")
+            :dob '("Custom validation failed for dob")
+            :year '("year must be a number" "year must be present")
             :name '("name must be present")
+            :age '("age must be a positive number")
             }
-           (first (core/validate {:age ""}
+           
+           (first (core/validate {:age -1 :year ""}
                                  :name core/required
-                                 :age [core/required core/number])))))
+                                 :year [core/required core/number]
+                                 :age core/positive
+                                 :dob (core/custom #(not (nil? %))))))))
 
   (testing "custom messages"
     (is (= {
             :age '("Idade deve ser maior que zero")
             :year '("Ano deve ser um numero" "Ano eh obrigatorio")
             :name '("Nome eh obrigatorio")
+            :dob  '("Nao pode ser nulo")
             }
            (first (core/validate {:age -1 :year ""}
                                  :name (core/required :message "Nome eh obrigatorio")
                                  :year [(core/required :message "Ano eh obrigatorio")
                                         (core/number :message "Ano deve ser um numero")]
-                                 :age (core/positive :message "Idade deve ser maior que zero"))
+                                 :age (core/positive :message "Idade deve ser maior que zero")
+                                 :dob (core/custom #(not (nil? %)) :message "Nao pode ser nulo"))
                   )))))
 
 (deftest validation-result
@@ -123,7 +123,7 @@
                       (nil? (:errors map))))))))
 
 
-(deftest array-validations
+(deftest coll-validations
   (let [valid-map   {:name "Leo" :pets [{:name "Aragorn"} {:name "Gandalf"}]}
         invalid-map {:name "Leo" :pets [{:name nil} {:name "Gandalf"}]}]
     
@@ -160,11 +160,11 @@
                            :address {:current { :country "Australia"}
                                      :past [{:country "Spain"} {:country nil}]}}
                           [:address :past] (core/every #(not (nil? (:country %)))))))))
-;;(require '[bouncer.core :as core] :reload)
+
 (deftest all-validations
   (testing "all built-in validators"
     (let [errors-map {
-                      :age '("age must be a number" "age must be present")
+                      :age '("age isn't 29" "age must be a number" "age must be present")
                       :name '("name must be present")
                       :passport {:number '("number must be a positive number")}
                       :address {:past '("All items in past must satisfy the predicate")}
@@ -177,6 +177,8 @@
       (is (= errors-map
              (first (core/validate invalid-map
                                    :name core/required
-                                   :age [core/required core/number]
+                                   :age [core/required
+                                         core/number
+                                         (core/custom #(= 29 %) :message "age isn't 29")]
                                    [:passport :number] core/positive 
                                    [:address :past] (core/every #(not (nil? (:country %)))))))))))
